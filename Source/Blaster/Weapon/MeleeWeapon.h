b68f7ffd917a00c4950f0c65b83274412ddab7f3
nilrithGitHub <nilrith14@gmail.com>
2023-03-10 18:20:09 +0700

    sa

--- Source/Blaster/Weapon/MeleeWeapon.h
+++ Source/Blaster/Weapon/MeleeWeapon.h
@@ -3,7 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Weapon/Weapon.h"
+#include "Weapon.h"
 #include "MeleeWeapon.generated.h"
 
 /**
@@ -13,5 +13,25 @@ UCLASS()
 class BLASTER_API AMeleeWeapon : public AWeapon
 {
 	GENERATED_BODY()
-	
+public:
+	virtual void Fire(const FVector& HitTarget) override;
+protected:
+
+	void WeaponTraceHit(const FVector& TraceStart, const FVector& TraceEnd, const FVector& HitTarget, FHitResult& OutHit);
+
+	UPROPERTY(EditAnywhere)
+		class UParticleSystem* ImpactParticles;
+
+	UPROPERTY(EditAnywhere)
+		USoundCue* HitSound;
+private:
+
+	UPROPERTY(EditAnywhere)
+		UParticleSystem* BeamParticles;
+
+	UPROPERTY(EditAnywhere)
+		UParticleSystem* MuzzleFlash;
+
+	UPROPERTY(EditAnywhere)
+		USoundCue* FireSound;
 };
