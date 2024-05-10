c983f4d1f69afb0c0fb5c57c99518f37a9585d7e
nilrithGitHub <nilrith14@gmail.com>
2024-05-10 12:28:55 +0700

    save

--- Source/Blaster/HUD/OverheadWidget.cpp
+++ Source/Blaster/HUD/OverheadWidget.cpp
@@ -35,8 +35,14 @@ void UOverheadWidget::ShowPlayerNetRole(APawn* InPawn)
 	SetDisplayText(RemoteRoleString);
 }
 
-void UOverheadWidget::OnLevelRemovedFromWorld(ULevel* InLevel, UWorld* InWorld)
+void UOverheadWidget::NativeDestruct()
 {
 	RemoveFromParent();
-	Super::OnLevelRemovedFromWorld(InLevel, InWorld);
+	Super::NativeDestruct();
 }
+
+//void UOverheadWidget::OnLevelRemovedFromWorld(ULevel* InLevel, UWorld* InWorld)
+//{
+//	RemoveFromParent();
+//	Super::OnLevelRemovedFromWorld(InLevel, InWorld);
+//}
